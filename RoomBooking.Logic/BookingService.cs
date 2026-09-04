92b93a17e1348ce0caa7dca870e2fa2ee8153199
2078-23 <cuddores@mail.ru>
2026-09-04 10:29:28 +0300

    Добавить слой логики и вывод результата

---
+++ RoomBooking.Logic/BookingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoomBooking.Data;
+
+namespace RoomBooking.Logic
+{
+    public class BookingService
+    {
+        private readonly BookingRepository _repository = new();
+
+        public List<Booking> GetImportant()
+        {
+            return _repository.GetAll()
+                .Where(item => item.Hour > 14)
+                .ToList();
+        }
+    }
+}
--- RoomBooking/Program.cs
+++ RoomBooking/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("RoomBooking Dubinin Rodion");
+﻿using RoomBooking.Logic;
+
+var service = new BookingService();
+Console.WriteLine("Отображенные записи: ");
+
+foreach (var item in service.GetImportant())
+{
+    Console.WriteLine($"{item.Id}: {item.Room}");
+}
